3457031aa1c947ef1b77d07673a8334516507bc5
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2023-04-25 17:13:48 -0300

    Create aula046 - TreasureMap.py

---
+++ exercicios/aula046 - TreasureMap.py
@@ -0,0 +1,7 @@
+linha1 = ['[ ]', '[ ]', '[ ]']
+linha2 = ['[ ]', '[ ]', '[ ]']
+linha3 = ['[ ]', '[ ]', '[ ]']
+mapa = [linha1, linha2, linha3]
+print(f'1 {linha1}\n2 {linha2}\n3 {linha3}')
+print('-' * 25)
+posicao_linha = int(input('Em qual linha deseja colocar o tesouro?\n'))
