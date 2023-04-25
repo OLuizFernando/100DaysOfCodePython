52f03b7a3074e5e17de206c2f1a91dd73f257bb8
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2023-04-25 17:42:13 -0300

    Update aula046 - TreasureMap.py

--- exercicios/aula046 - TreasureMap.py
+++ exercicios/aula046 - TreasureMap.py
@@ -2,6 +2,28 @@ linha1 = ['[ ]', '[ ]', '[ ]']
 linha2 = ['[ ]', '[ ]', '[ ]']
 linha3 = ['[ ]', '[ ]', '[ ]']
 mapa = [linha1, linha2, linha3]
-print(f'1 {linha1}\n2 {linha2}\n3 {linha3}')
+print(f'\033[34m1\033[m {linha1}\n\033[34m2\033[m {linha2}\n\033[34m3\033[m {linha3}')
 print('-' * 25)
-posicao_linha = int(input('Em qual linha deseja colocar o tesouro?\n'))
+while True:
+    posicao_linha = int(input('Em qual linha deseja colocar o tesouro? '))
+    if 1 <= posicao_linha <= 3:
+        break
+    else:
+        print('\033[31mERRO: Digite um número entre 1 e 3.\033[m')
+print('\033[34m   1      2      3\033[m')
+if posicao_linha == 1:
+    print(f'\033[34m{linha1}\033[m\n{linha2}\n{linha3}')
+elif posicao_linha == 2:
+    print(f'{linha1}\n\033[34m{linha2}\033[m\n{linha3}')
+else:
+    print(f'{linha1}\n{linha2}\n\033[34m{linha3}\033[m')
+print('-' * 25)
+while True:
+    posicao_coluna = int(input('Em qual coluna deseja colocar o tesouro? '))
+    if 1 <= posicao_coluna <= 3:
+        break
+    else:
+        print('\033[31mERRO: Digite um número entre 1 e 3.\033[m')
+mapa[posicao_linha - 1][posicao_coluna - 1] = '[X]'
+print('     1      2      3')
+print(f'1 {linha1}\n2 {linha2}\n3 {linha3}')
