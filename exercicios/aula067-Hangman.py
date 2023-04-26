b39822850d90b7e09b922716f1f68ced30781250
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2023-04-26 20:50:51 -0300

    Update aula067-Hangman.py

--- exercicios/aula067-Hangman.py
+++ exercicios/aula067-Hangman.py
@@ -4,17 +4,22 @@ lista_palavras = ['python', 'desenvolvimento', 'computador', 'aprendizado', 'sof
 espacos = []
 vidas = 6
 palavra = rd.choice(lista_palavras)
-for letra in palavra:
+for c in range(len(palavra)):
     espacos.append('_')
-for espaco in espacos:
-    print(espaco, end=' ')
-chute = str(input('\nDigite uma letra: ')).lower().strip()[0]
-for c in range(0, len(palavra)):
-    if chute == palavra[c]:
-        espacos[c] = chute
-    elif chute not in palavra:
-        print(f'A letra "{chute}" não está na palavra.')
-        vidas -= 1
-        break
-for espaco in espacos:
-    print(espaco, end=' ')
+    print(espacos[c], end=' ')
+print()
+while vidas > 0 and '_' in espacos:
+    chute = str(input('Digite uma letra: ')).lower().strip()[0]
+    for c in range(len(palavra)):
+        if chute == palavra[c]:
+            espacos[c] = chute
+        elif chute not in palavra:
+            print(f'A letra "{chute}" não está na palavra.')
+            vidas -= 1
+            print(f'Agora você tem {vidas} vidas.')
+            break
+    if chute in palavra and '_' in espacos:
+        for espaco in espacos:
+            print(espaco, end=' ')
+        print()
+print(f'Você venceu! A palavra é {palavra}')
