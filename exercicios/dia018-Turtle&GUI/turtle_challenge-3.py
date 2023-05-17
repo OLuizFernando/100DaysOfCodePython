c35a74594ba318163d44fb0b02b9a9aab40a8972
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2023-05-16 21:23:34 -0300

    update turtle exercises

---
+++ exercicios/dia018-Turtle&GUI/turtle_challenge-3.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+t = Turtle()
+t.shape('turtle')
+t.speed(1000)
+s = Screen()
+angulo = 3
+t.penup()
+t.left(90)
+t.forward(400)
+t.right(90)
+t.pendown()
+while True:
+    for _ in range(angulo):
+        t.right(360 / angulo)
+        t.forward(50)
+    angulo += 1
+s.exitonclick()
