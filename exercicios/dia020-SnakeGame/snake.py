d1226fe3d0a3279f78ba9b9eb92e964b23246cd4
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2023-07-05 23:03:55 -0300

    snake doesn't walk backward

--- exercicios/dia020-SnakeGame/snake.py
+++ exercicios/dia020-SnakeGame/snake.py
@@ -8,9 +8,11 @@ class Cobra:
         self.criar_cobra()
         self.cabeca = self.corpo[0]
 
-        # Cria a cobra, segmento por segmento e os coloca na posição inicial
+        # cria a cobra, segmento por segmento e os coloca na posição inicial
     def criar_cobra(self):
-        for pos in range(0, -101, -20):
+        # cada segmento quadrado tem 20 x 20  de tamanho
+        # a estrutura for irá criar 3 segmentos, 1° na posição 0, 2° na -20 e 3° na -40
+        for pos in range(0, -41, -20):
             segmento = Turtle('square')
             segmento.color('#2C301D')
             segmento.penup()
@@ -20,19 +22,25 @@ class Cobra:
     def mover(self):
         # faz a cobra se mover constantemente
         for segmento in range(len(self.corpo) - 1, 0, -1):
-            # os segmentos do corpo acompanham o da cabeça
+            # os segmentos de trás acompanham o segmento da frente (cabeça)
             self.corpo[segmento].goto(x=self.corpo[segmento - 1].xcor(), y=self.corpo[segmento - 1].ycor())
         self.cabeca.forward(20)
 
-    # Define cada função para direcionar a cabeça da cobra
+    # define cada função para direcionar a cabeça da cobra
+    # só executa o comando se a direção da cobra não for a direção contrária, assim a cobra não dá ré
     def cima(self):
-        self.cabeca.setheading(90)
+
+        if self.cabeca.heading() != 270:
+            self.cabeca.setheading(90)
 
     def esquerda(self):
-        self.cabeca.setheading(180)
+        if self.cabeca.heading() != 0:
+            self.cabeca.setheading(180)
 
     def baixo(self):
-        self.cabeca.setheading(270)
+        if self.cabeca.heading() != 90:
+            self.cabeca.setheading(270)
 
     def direita(self):
-        self.cabeca.setheading(0)
+        if self.cabeca.heading() != 180:
+            self.cabeca.setheading(0)
