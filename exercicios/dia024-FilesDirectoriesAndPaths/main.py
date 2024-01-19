9ab72b03379d1cf207dc73f85ef9e40415683049
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-18 23:12:44 -0300

    removing newlines of names list

--- exercicios/dia024-FilesDirectoriesAndPaths/main.py
+++ exercicios/dia024-FilesDirectoriesAndPaths/main.py
@@ -1,5 +1,6 @@
 with open('./Names/invited_names.txt', mode='r', encoding='utf-8') as arquivo:
     lista = arquivo.readlines()
+lista = list(map(lambda a:a.strip(), lista))
 
 with open('./Letters/starting_letter.txt', mode='r', encoding='utf-8') as arquivo:
     convite = arquivo.read()
@@ -7,8 +8,8 @@ with open('./Letters/starting_letter.txt', mode='r', encoding='utf-8') as arquiv
 mensagens_prontas = []
 
 for i in range(0, len(lista)):
-    with open(f'./Output/ReadyToSend/convite_para_{i}', mode='w', encoding='utf-8') as arquivo:
-        arquivo.write(convite.replace('[nome]', lista[i]))
+    with open(f'./Output/ReadyToSend/convite_para_{i}.txt', mode='w', encoding='utf-8') as arquivo:
+        arquivo.write(convite.replace('[nome]', f'{lista[i]}'))
 
 
 
