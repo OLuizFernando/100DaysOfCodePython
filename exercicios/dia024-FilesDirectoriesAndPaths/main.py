094ee93e885b79462ebc9127a263ac490d635cb6
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-19 00:37:40 -0300

    finish day25

--- exercicios/dia024-FilesDirectoriesAndPaths/main.py
+++ exercicios/dia024-FilesDirectoriesAndPaths/main.py
@@ -1,15 +1,9 @@
 with open('./Names/invited_names.txt', mode='r', encoding='utf-8') as arquivo:
-    lista = arquivo.readlines()
-lista = list(map(lambda a:a.strip(), lista))
+    lista = arquivo.read().splitlines()
 
 with open('./Letters/starting_letter.txt', mode='r', encoding='utf-8') as arquivo:
     convite = arquivo.read()
 
-mensagens_prontas = []
-
 for i in range(0, len(lista)):
-    with open(f'./Output/ReadyToSend/convite_para_{i}.txt', mode='w', encoding='utf-8') as arquivo:
-        arquivo.write(convite.replace('[nome]', f'{lista[i]}'))
-
-
-
+    with open(f'./Output/ReadyToSend/convite_para_{lista[i].lower()}', mode='w', encoding='utf-8') as arquivo:
+        arquivo.write(convite.replace('[nome]', lista[i]))
