24991ef9b3849c29b1278cbaf5970c511f10ca2f
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-20 23:25:12 -0300

    prettytable and csv library test

--- exercicios/dia025-CSVDataAndPandasLibrary/csv_library.py
+++ exercicios/dia025-CSVDataAndPandasLibrary/csv_library.py
@@ -2,5 +2,9 @@ import csv
 
 with open('dados_pessoas.csv') as arquivo:
     ficha = csv.reader(arquivo)
+    idades = []
     for linha in ficha:
-        print(linha)
+        if linha[1].isnumeric():
+            idades.append(int(linha[1]))
+
+print(idades)
