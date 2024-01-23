819668f6b29cf3fba14a1397e98b323ce697748e
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-23 18:46:40 -0300

    coordinates fixed

--- exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
+++ exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
@@ -1,16 +1,26 @@
 import turtle as t
+import pandas
 
 s = t.Screen()
+s.tracer(0)
 s.title('Brazil States Game')
-imagem = 'mapa.gif'
-s.addshape(imagem)
-t.shape(imagem)
+s.bgpic('map.gif')
+t.penup()
+t.hideturtle()
 
+data_df = pandas.read_csv('27_states.csv')
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
+# for i in range(len(data_df)):
+#     t.goto(x=data_df.x[i], y=data_df.y[i])
+#     t.write(data_df.state[i], align='center', font=('Arial', 9, 'bold'))
+#     s.update()
 
+user_answer = s.textinput(title='Guess a State', prompt='Enter the name of a state in Brazil:')
 
-t.onscreenclick(get_mouse_click_coor)
+if user_answer in list(data_df.state):
+    # t.goto(x=int(data_df[data_df.state == user_answer].x), y=int(data_df[data_df.state == user_answer].y))
+    # t.write(data_df[data_df.state == user_answer].state, align='center', font=('Arial', 9, 'bold'))
+    # s.update()
+    print(data_df.loc[???])
 
 t.mainloop()
