5cf01929fdaf72f71a3b038a56114d0d85d43347
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-29 01:28:38 -0300

    br_states_game final version

--- exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
+++ exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
@@ -16,17 +16,17 @@ data_df = pandas.read_csv('27_states.csv')
 #     s.update()
 
 guesses = []
-score = 0
+score = len(guesses)
 
 game_is_on = True
 while game_is_on:
 
     user_answer = s.textinput(title=f'{score}/27 States Correct',
-                              prompt='Enter the name of a state in Brazil:')
+                              prompt='Enter the name of a state in Brazil:')\
+                              .title().replace('Do', 'do').replace('De', 'de').replace('Df', 'DF')
 
     if user_answer in list(data_df.state) and user_answer not in guesses:
 
-        score += 1
         guesses.append(user_answer)
 
         t.goto(x=int(data_df[data_df.state == user_answer].iloc[0].x),
@@ -39,5 +39,3 @@ while game_is_on:
 
         if score >= 27:
             game_is_on = False
-
-t.mainloop()
