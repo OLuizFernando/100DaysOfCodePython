29680e31471c3d83d9b112c1cc2d78c8947a2b54
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-02-04 20:31:01 -0300

    br_states_game final version

--- exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
+++ exercicios/dia025-CSVDataAndPandasLibrary/br_states_game/main.py
@@ -10,32 +10,41 @@ t.hideturtle()
 
 data_df = pandas.read_csv('27_states.csv')
 
-# for i in range(len(data_df)):
-#     t.goto(x=data_df.x[i], y=data_df.y[i])
-#     t.write(data_df.state[i], align='center', font=('Arial', 9, 'bold'))
-#     s.update()
-
 guesses = []
-score = len(guesses)
 
 game_is_on = True
 while game_is_on:
 
-    user_answer = s.textinput(title=f'{score}/27 States Correct',
-                              prompt='Enter the name of a state in Brazil:')\
-                              .title().replace('Do', 'do').replace('De', 'de').replace('Df', 'DF')
+    try:
+        user_answer = s.textinput(title=f'{len(guesses)}/27 States Correct',
+                                  prompt='Enter the name of a state in Brazil:')\
+                                .title().replace('Do', 'do').replace('De', 'de')\
+                                .replace('Df', 'DF').replace('Distrito Federal', 'DF')
 
-    if user_answer in list(data_df.state) and user_answer not in guesses:
+        if user_answer in list(data_df.state) and user_answer not in guesses:
 
-        guesses.append(user_answer)
+            guesses.append(user_answer)
 
-        t.goto(x=int(data_df[data_df.state == user_answer].iloc[0].x),
-               y=int(data_df[data_df.state == user_answer].iloc[0].y))
+            state_data = data_df[data_df.state == user_answer]
 
-        index = data_df[data_df.state == user_answer].index[0]
-        t.write(data_df.state[index], align='center', font=('Arial', 9, 'bold'))
+            t.goto(x=int(state_data.x.iloc[0]), y=int(state_data.y.iloc[0]))
+            t.write(state_data.state.item(), align='center', font=('Arial', 9, 'bold'))
 
-        s.update()
+            s.update()
 
-        if score >= 27:
-            game_is_on = False
+            if len(guesses) >= 27:
+                game_is_on = False
+                print('\033[32mYou won the game.\033[m')
+
+    except AttributeError:
+        game_is_on = False
+        print('\033[31mYou left the game.\033[m')
+
+        missing_states = []
+        for state in list(data_df.state):
+            if state not in guesses:
+                missing_states.append(state)
+
+        print('\nThe following states were missing:')
+        for state in sorted(missing_states):
+            print(state)
