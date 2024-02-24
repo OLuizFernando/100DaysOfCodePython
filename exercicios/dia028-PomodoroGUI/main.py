944492b82e1303b0c6ef68945f0580f1ac9e17a2
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-02-24 00:22:35 -0300

    pomodoro gui alpha version

--- exercicios/dia028-PomodoroGUI/main.py
+++ exercicios/dia028-PomodoroGUI/main.py
@@ -2,29 +2,44 @@ from tkinter import *
 from time import sleep
 
 
-def contador(minutos, segundos=0):
-    segundos += minutos * 60
-    for i in range(segundos):
-        mins, secs = divmod(segundos - i, 60)
-        time.config(text=f'{mins:02d}:{secs:02d}')
-        sleep(1)
-        window.update()
+def countdown(minutes, seconds=0):
+    seconds += minutes * 60
+    for i in range(seconds + 1):
+        global keep_counting
+        if keep_counting:
+            mins, secs = divmod(seconds - i, 60)
+            time.config(text=f'{mins:02d}:{secs:02d}')
+            sleep(1)
+            window.update()
+        else:
+            main_label.config(text='Timer')
 
 
 def work_time():
-    main_label.config(text='Work')
-    contador(25)
+    global keep_counting
+    keep_counting = True
+    for i in range(4):
+        main_label.config(text='Work')
+        countdown(25)
+        if i < 3:
+            break_time(5)
+        else:
+            break_time(15)
 
 
-def break_time():
+def break_time(mins, secs=0):
     main_label.config(text='Break')
-    contador(5)
+    countdown(mins, secs)
 
 
 def reset_time():
+    global keep_counting
+    keep_counting = False
     main_label.config(text='Timer')
     time.config(text='00:00')
 
+
+keep_counting = True
 
 window = Tk()
 window.title('Pomodoro')
@@ -43,9 +58,6 @@ time.grid(column=1, row=1)
 
 start_button = Button(window, text='Start', font=('Small Fonts', 20), width=6, command=work_time)
 start_button.grid(column=0, row=2)
-
-break_button = Button(window, text='Break', font=('Small Fonts', 20), width=6, command=break_time)
-break_button.grid(column=1, row=2)
 
 reset_button = Button(window, text='Reset', font=('Small Fonts', 20), width=6, command=reset_time)
 reset_button.grid(column=2, row=2)
