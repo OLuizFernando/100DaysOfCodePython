0dee75878322d5c2cea579234eae374c7d2bd8f6
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-02-24 00:45:35 -0300

    layout fix

--- exercicios/dia028-PomodoroGUI/main.py
+++ exercicios/dia028-PomodoroGUI/main.py
@@ -43,23 +43,30 @@ keep_counting = True
 
 window = Tk()
 window.title('Pomodoro')
+window.minsize(width=710, height=645)
 window.config(padx=30, pady=30)
 window.resizable(width=FALSE, height=FALSE)
 
 main_label = Label(window, text='Timer', font=('Small Fonts', 50, 'bold'))
-main_label.grid(column=1, row=0)
+main_label.place(x=235, y=0)
+# main_label.grid(column=1, row=0)
 
 image = PhotoImage(file='tomato.png')
 tomato = Label(window, image=image)
-tomato.grid(column=1, row=1, padx=20, pady=20)
+tomato.place(x=125, y=100)
+# tomato.grid(column=1, row=1, padx=20, pady=20)
 
 time = Label(window, text='00:00', font=('Small Fonts', 30, 'bold'), background='#c1c1c1')
-time.grid(column=1, row=1)
+time.place(x=265, y=320)
+# time.grid(column=1, row=1)
 
 start_button = Button(window, text='Start', font=('Small Fonts', 20), width=6, command=work_time)
-start_button.grid(column=0, row=2)
+start_button.place(x=27, y=525)
+# start_button.grid(column=0, row=2)
 
 reset_button = Button(window, text='Reset', font=('Small Fonts', 20), width=6, command=reset_time)
-reset_button.grid(column=2, row=2)
+reset_button.place(x=519, y=525)
+# reset_button.grid(column=2, row=2)
+
 
 window.mainloop()
